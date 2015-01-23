76f071a3611130ef0706c57b5f063141e9310583
Nathan G <nathan@monochrome.co.uk>
2015-01-23 17:05:13 +0000

    tidy

--- source/javascripts/components/calculator.js
+++ source/javascripts/components/calculator.js
@@ -1,36 +1,7 @@
-define(['knockout', 'text!/components/calculator.html', 'pathway'], function(ko, html, Pathway) {
+define(['knockout', 'text!/components/calculator.html', 'pathway'],
+  function(ko, html, Pathway) {
+
   'use strict';
-
-  /** Constants for pathway */
-  var PATHWAY_DATA = {
-    actions: [
-      { category: 1 }
-    ]
-  };
-
-  /**
-   * Represents a pathway route
-   *
-   * @param {object} args - arguments object
-   * @param {string} args.longCode - Actual 2050 code
-   * @param {string} args.description - Short description of pathway actions
-   * @param {number} args.categoryId - PathwayRouteCategory id
-   */
-  var PathwayRoute = function(args) {
-    var self = this;
-
-    self.longCode = args.longCode;
-    self.description = args.description || null;
-    self.name = args.name || null;
-    self.categoryId = args.categoryId || null;
-  };
-
-  /** For route lookup inflection */
-  var PATHWAY_ROUTES = {
-  //   new PathwayRoute({
-  //      longCode: '10111111111111110111111001111110111101101101110110111',
-  //      description: 'Dosent tackle climate change' })
-  };
 
   /**
    * The main app view model. Tracks app state
