342e64d1cf133d3fc5a924773efeee33c1ed47c6
Nathan G <nathan@monochrome.co.uk>
2015-01-23 15:44:38 +0000

    add next button

--- source/javascripts/bindings/scrolls.js
+++ source/javascripts/bindings/scrolls.js
@@ -3,10 +3,12 @@ define(['knockout', 'scroller'], function(ko, Scroller) {
 
   ko.bindingHandlers.scrolls = {
     init: function(el, valueAccessor) {
-      new Scroller(el, { nav: true });
+      new Scroller(el, { nav: true, next: true });
     },
 
-    update: function(el, valueAccessor) {}
+    update: function(el, valueAccessor) {
+      // hide / show next button
+    }
   };
 });
 
