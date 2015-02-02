10fd91ba1476d9c64b24a99f76d470c231585b58
Nathan G <nathan@monochrome.co.uk>
2015-02-02 10:14:00 +0000

    demo menu

--- source/javascripts/components/mainNav.js
+++ source/javascripts/components/mainNav.js
@@ -1,7 +1,20 @@
-define(['knockout', 'text!/components/main-nav.html'], function(ko, html) {
+define(['knockout', 'text!/components/main-nav.html', 'ajax'], function(ko, html, Ajax) {
   'use strict';
 
-  var ViewModel = function() {};
+  var ViewModel = function() {
+    var self = this;
+
+    self.exampleCategories = ko.observableArray();
+
+    Ajax.request({
+      url: '/data/examples.json',
+      onSuccess: function(xhr) {
+        var response = JSON.parse(xhr.responseText);
+
+        self.exampleCategories(response.exampleCategories);
+      }
+    });
+  };
 
   return {
     viewModel: ViewModel,
