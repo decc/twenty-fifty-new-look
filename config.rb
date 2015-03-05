bd24b952178f31663aaaf5268402740723106618
Alan Cole <me@alancole.io>
2015-03-05 11:20:54 -0500

    stopping components loading layout.slim to cut down on page bloat and css issues

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@
 # Per-page layout changes:
 #
 # With no layout
-page "/components/**.*.html", layout: false
+page "/components/*", layout: false
 #
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
