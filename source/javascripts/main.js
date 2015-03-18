b13b1a9abdf66a487db085f3cfde4f9965b3b9d6
Jolyon <email@person.com>
2015-03-18 18:44:41 +0000

    Polyfills

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -13,11 +13,21 @@
       scroller: 'vendor/scroller',
       text: 'vendor/text',
       d3: 'vendor/d3',
-      tabber: 'vendor/tabber'
+      tabber: 'vendor/tabber',
+      modernizr: 'vendor/modernizr'
     }
   });
 
-  require(['app', 'componentLoader', 'customElements'], function(app) {
+  require(['app', 'vendor/modernizr', 'componentLoader', 'customElements'], function(app) {
+  //     Modernizr.load({
+  //   test : Modernizr.geolocation,
+  //   yep  : 'normal.js',
+  //   nope : ['polyfill.js', 'wrapper.js']
+  // });
+
+    Modernizr.load({load: ['/javascripts/vendor/polyfills/classList.js']});
+    Modernizr.load({load: ['/javascripts/vendor/polyfills/es5.js']});
+
     app.init();
   });
 })(require, define);
