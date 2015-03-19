0df3f2a30a28cb4dfb0c364d7c5f0ad174d8ee29
Alan Cole <me@alancole.io>
2015-03-19 07:10:41 -0400

    finished faq animations

--- source/javascripts/bindings/faqs.js
+++ source/javascripts/bindings/faqs.js
@@ -8,7 +8,13 @@ define(['knockout'], function(ko) {
         var ele = event.target;
         var header = ele.parentNode;
         var article = header.parentNode;
-
+        
+        // close all the open one
+        var e = document.getElementsByClassName('faqs')[0].getElementsByClassName('active');
+        if(e.length > 0 && !e[0].isEqualNode(article)){
+          e[0].className = e[0].className.replace( /(?:^|\s)active(?!\S)/g ,'');
+        }
+        
         if(article.className.match(/(?:^|\s)active(?!\S)/)){
           // remove class
           article.className = article.className.replace( /(?:^|\s)active(?!\S)/g ,'');
@@ -16,7 +22,7 @@ define(['knockout'], function(ko) {
           // add class
           article.className += " active";
         }
-
+        
       });
     }
   };
