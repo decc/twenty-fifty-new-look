322b4a878443a14bafd710c3b2f34e40861e0c53
Nathan G <nathan@monochrome.co.uk>
2015-03-19 17:55:42 +0000

    tidy

--- source/javascripts/components/chartViews/energySecurity.js
+++ source/javascripts/components/chartViews/energySecurity.js
@@ -10,10 +10,6 @@ define(['knockout', 'text!/components/chartViews/energySecurity.html'],
       // TODO: why is chartData null sometimes
       var chartData = args.data()[args.charts[0].name] || { imports: [], diversity: [], electricity: {}};
 
-      if(!args.data()[args.charts[0].name]) {
-        console.dir(args.data())
-      }
-
       return chartData;
     });
 
