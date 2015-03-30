d683de605312fd136306da96a84ec08f34534f7e
Nathan G <nathan@monochrome.co.uk>
2015-03-30 11:13:50 +0100

    url

--- source/javascripts/config.js
+++ source/javascripts/config.js
@@ -5,6 +5,6 @@ define([],
   return {
     'apiUrl': "http://obscure-sierra-8645.herokuapp.com",
     // 'apiUrl': "http://2050-calculator-tool.decc.gov.uk",
-    'siteUrl': "https://decc-2050.herokuapp.com",
+    'siteUrl': "http://staging.decc.monochromedns.co.uk",
   };
 });
