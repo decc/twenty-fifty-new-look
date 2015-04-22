4488248cebf6abd4e13047ca57433d6f7ce7dbf0
Nathan G <nathan@monochrome.co.uk>
2015-04-22 16:15:55 +0100

    make splash and guide work in small and large screens [Completed: #89] [touch: #85]

--- source/javascripts/bindings/scrolls.js
+++ source/javascripts/bindings/scrolls.js
@@ -6,6 +6,20 @@ define(['knockout', 'scroller'], function(ko, Scroller) {
       var scroller = new Scroller(el, { nav: true, next: true });
       scroller._activateCurrent();
 
+      var activeClass = 'is-active';
+
+      // todo: this should not be coupled to scroll binding
+      scroller.on('activatesection', function() {
+        var logo = document.getElementById('fixed-logo');
+        var slideId = this.currentSectionId;
+
+        if(slideId === 0) {
+          logo.classList.add(activeClass);
+        } else {
+          logo.classList.remove(activeClass);
+        }
+      });
+
       ko.utils.domNodeDisposal.addDisposeCallback(el, function() {
         scroller.destroy();
       });
