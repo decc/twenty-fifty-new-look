ba531841855ec17c97d94d35ff9f42d811927f63
Nathan G <nathan@monochrome.co.uk>
2015-06-16 15:12:37 +0100

    better instructions

--- source/javascripts/config.js
+++ source/javascripts/config.js
@@ -6,10 +6,11 @@ define([],
     MIN_WIDTH: 600,
     MIN_HEIGHT: 400,
     ROTATE_WIDTH: 700,
-    apiUrl: "http://obscure-sierra-8645.herokuapp.com",
+    // apiUrl: "http://obscure-sierra-8645.herokuapp.com",
     // apiUrl: "http://2050-calculator-tool.decc.gov.uk",
+    apiUrl: (function() { return "http://", window.location.host].join('') } )()
     // siteUrl: "http://localhost:4567"
-    siteUrl: "http://staging.decc.monochromedns.co.uk"
+    siteUrl: (function() { return "http://", window.location.host].join('') } )()
   };
 });
 
