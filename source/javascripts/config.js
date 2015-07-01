898df6761d75643ff32967329b6646362cf1ffa6
Tom Counsell <tamc@greenonblack.com>
2015-07-01 12:09:11 +0200

    Fix error in config.js

--- source/javascripts/config.js
+++ source/javascripts/config.js
@@ -8,9 +8,9 @@ define([],
     ROTATE_WIDTH: 700,
     // apiUrl: "http://obscure-sierra-8645.herokuapp.com",
     // apiUrl: "http://2050-calculator-tool.decc.gov.uk",
-    apiUrl: (function() { return "http://", window.location.host].join('') } )()
+    apiUrl: (function() { return "http://"+window.location.host }()),
     // siteUrl: "http://localhost:4567"
-    siteUrl: (function() { return "http://", window.location.host].join('') } )()
+    siteUrl: (function() { return "http://"+window.location.host }())
   };
 });
 
