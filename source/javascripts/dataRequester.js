beccac0edfeea4d11a1bd625fadbaa167270b978
tamc <tamc@greenonblack.com>
2015-07-20 08:00:57 +0000

    Remove the cross domain request requirement from data requests
    The data request is no longer cross domain. On IE9 requiring a cross domain
    request when the request is on the local domain causes a security violation.

--- source/javascripts/dataRequester.js
+++ source/javascripts/dataRequester.js
@@ -6,7 +6,6 @@ define(['ajax', 'config'], function(Ajax, config) {
   DataRequester.pathway = function(pathwayString, callback) {
     Ajax.request({
       url: config.apiUrl + '/pathways/' + pathwayString + '/data',
-      XDomainRequest: true,
       onSuccess: callback,
       onError: function(){
         alert("Sorry, there was an error connecting to the server.")
